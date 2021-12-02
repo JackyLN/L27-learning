45d15c8dc86c1604184b7e1eabe183bc9d1a3014
Lien Trinh <lientrinh@Liens-MacBook-Air.local>
2021-12-02 21:35:35 +0900

    commit by Lien

--- myscript.js
+++
@@ -1,8 +0,0 @@
-function alertme() {
-  alert("Lien xinh dep hehe")
-}
-
-function changecolor(){
-  document.getElementById("selfexplain").style.color = "orange";
-  document.getElementById("selfexplain").innerHTML ="Noi lien xinh dep di"
-}
